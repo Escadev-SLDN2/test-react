09b59fd60ff0289a70ed091cec8b94c625a43d6c
Nicolas Vauché <hello@nicolasvauche.net>
2020-07-09 17:13:37 +0200

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
 import Home from './Home'
 import Contact from './Contact'
@@ -7,21 +8,25 @@ import './assets/css/index.css'
 
 function App () {
   return (
-    <>
+    <BrowserRouter>
       <header>
         <h1>Bienvenue sur mon application React !</h1>
         <nav>
           <ul>
             <li>
-              <a href=''>Accueil</a>
+              <Link to='/'>Accueil</Link>
             </li>
             <li>
-              <a href=''>Contact</a>
+              <Link to='/contact'>Contact</Link>
             </li>
           </ul>
         </nav>
       </header>
-    </>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
