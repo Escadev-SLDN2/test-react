98df93224b2ed453241c129e4986ef176b9a3333
Bastien BALOUP <35816948+Bastien-Baloup@users.noreply.github.com>
2020-07-09 15:52:29 +0200

    click handler

--- src/header.jsx
+++ src/header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
 class Header extends React.Component {
+
+    state = {   
+        prenom:     "test",
+        nom:        "blabla",
+        profession: "blablabla",
+    }
+
+    clickHandler = (event) => {
+        this.setState({prenom: "test2" })
+    }
+
     render(_props) {
         return(
             <header>
                 <h1>Bienvenue sur mon application React, {this.props.prenom} !</h1>
+                <p>
+                    {this.state.prenom} {this.state.nom}, {this.state.profession}
+                    <button onClick={this.clickHandler}>change prenom</button>
+                </p>
             </header>
         )
     }
