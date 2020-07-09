bfad31341774a3cf29b90d58c2bc11e76a14f0cb
Nicolas Vauché <hello@nicolasvauche.net>
2020-07-09 16:47:40 +0200

    Ex5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+
+
 import './assets/css/index.css'
 
 function App () {
   return (
     <header>
       <h1>Bienvenue sur mon application React !</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href=''>Accueil</a>
+          </li>
+          <li>
+            <a href=''>Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
+
+    
   )
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      <h2>Accueil</h2>
+    </main>
+  )
+}
+
+export default Home
