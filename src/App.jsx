1f9600f0d773d22eb8544ed8657ef9a1e05a6b40
Nicolas Vauché <hello@nicolasvauche.net>
2020-07-09 10:58:59 +0200

    Prepare for ex

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './assets/css/index.css'
+
+function App () {
+  return (
+    <header>
+      <h1>Bienvenue sur mon application React !</h1>
+    </header>
+  )
+}
+
+export default App
