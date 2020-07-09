9624005d8d5f1fe2fd55b0203fddab087ae08ec0
Nicolas Vauché <hello@nicolasvauche.net>
2020-07-09 16:55:37 +0200

    fix end

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,21 @@ import './assets/css/index.css'
 
 function App () {
   return (
-    <header>
-      <h1>Bienvenue sur mon application React !</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href=''>Accueil</a>
-          </li>
-          <li>
-            <a href=''>Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
-    
+    <>
+      <header>
+        <h1>Bienvenue sur mon application React !</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href=''>Accueil</a>
+            </li>
+            <li>
+              <a href=''>Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
 
