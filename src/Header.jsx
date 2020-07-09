20465c1a5f0f316cd27e4e003345af2985d80075
Désir Thomas <thomasdesir.pro@gmail.com>
2020-07-09 11:21:32 +0200

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './assets/css/index.css'
+
+function Header () {
+  return (
+    <header>
+      <h1>Bienvenue sur mon application React !</h1>
+    </header>
+  )
+}
+
+export default Header
